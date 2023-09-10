0cd5bf1f5faece3558ff55bee3c18f46ab2c9cf1
MatasAdmoneo <120173596+MatasAdmoneo@users.noreply.github.com>
2023-09-11 01:02:29 +0300

    Db not added yet

--- Cf.WebApi/Program.cs
+++ Cf.WebApi/Program.cs
@@ -1,9 +1,22 @@
+using Cf.Infrastucture;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services.AddDbContext<CfContext>(options =>
+{
+    var connectionString = configuration["ConnectionStrings:CfDatabase"];
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
@@ -13,32 +26,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+await using var scope = app.Services.CreateAsyncScope();
+await using var db = scope.ServiceProvider.GetService<CfContext>();
+await db.Database.MigrateAsync();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
