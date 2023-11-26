b4b03e511b536adb77471d81b9320c12965b47f3
Ech0ke <aidas.zygas@gmail.com>
2023-11-26 15:55:20 +0200

    Refactor BE

--- be/Cf.WebApi/Endpoints/ServiceDocumentEndpoints.cs
+++ be/Cf.WebApi/Endpoints/ServiceDocumentEndpoints.cs
@@ -1,4 +1,6 @@
 ﻿using Cf.Application.Services.Interfaces;
+using Cf.Domain.Exceptions.Messages;
+using Cf.Domain.Exceptions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,24 +19,7 @@ public static class ServiceDocumentEndpoints
             .WithTags(Tag)
         .HasApiVersion(1);
 
-        group.MapPost("UploadPdf", UploadPdfAsync);
         group.MapPost("UploadpdfBytes", UploadPdfBytesAsync);
-    }
-
-    [Authorize(Roles = "Service")]
-    private static async Task UploadPdfAsync([FromServices] IServiceDocumentService pdfService, IHttpContextAccessor httpContextAccessor, [FromForm] IFormFile file)
-    {
-        if (file == null || file.Length == 0)
-        {
-            throw new ApplicationException();
-        }
-
-        // Add your logic to save the PDF file using the pdfService
-        // For example, you can save it to a specific folder or a database
-        var pdfBytes = await ReadFileBytesAsync(file);
-
-        // Your PDF service method to save the PDF
-        await pdfService.SavePdfAsync(pdfBytes, GetServiceId(httpContextAccessor));
     }
 
     [Authorize(Roles = "Service")]
@@ -42,28 +27,17 @@ public static class ServiceDocumentEndpoints
     {
         if (string.IsNullOrEmpty(base64Content))
         {
-            throw new ApplicationException("Base64 content is null or empty.");
+            throw new BadRequestException(DomainErrors.Service.NotFound);
         }
 
         try
         {
             byte[] pdfBytes = Convert.FromBase64String(base64Content);
-
-            // Your PDF service method to save the PDF
             await pdfService.SavePdfAsync(pdfBytes, GetServiceId(httpContextAccessor));
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            throw new ApplicationException("Failed to process the PDF content.", ex);
-        }
-    }
-
-    private static async Task<byte[]> ReadFileBytesAsync(IFormFile file)
-    {
-        using (var memoryStream = new MemoryStream())
-        {
-            await file.CopyToAsync(memoryStream);
-            return memoryStream.ToArray();
+            throw new InternalException(DomainErrors.Service.FailedUpload);
         }
     }
 
