e8f770daf4d5906fa262de2c997f000566cce91c
Ech0ke <aidas.zygas@gmail.com>
2023-12-06 19:53:17 +0200

    Add protected routes using middleware

--- fe/src/middleware.ts
+++ fe/src/middleware.ts
@@ -1,5 +1,25 @@
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
-export default withMiddlewareAuthRequired();
+import { NextRequest, NextResponse } from "next/server";
+import { getRole } from "./utils/getRole";
+export default withMiddlewareAuthRequired(async function middleware(req) {
+  const user = await getRole();
+
+  const roles = user["https://CarFans.com/roles"];
+  // service only routes
+  if (req.nextUrl.pathname.startsWith("/service")) {
+    if (roles.length === 0 || !roles.includes("Service")) {
+      return NextResponse.redirect(req.nextUrl.origin);
+    }
+  }
+
+  // client only routes
+  if (req.nextUrl.pathname.startsWith("/advert")) {
+    if (roles.length === 0 || !roles.includes("User")) {
+      return NextResponse.redirect(req.nextUrl.origin);
+    }
+  }
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: ["/dashboard", "/service/:path*"],
