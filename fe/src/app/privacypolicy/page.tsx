807252519bfdc18ddd2244853a19340b2453c117
AthenaKas <atene.kas@gmail.com>
2023-12-11 22:38:58 +0200

    Privacy Policy try dos

--- fe/src/app/privacypolicy/page.tsx
+++ fe/src/app/privacypolicy/page.tsx
@@ -1,71 +1,39 @@
 function PrivacyPolicy() {
     return (
-      <main>
-      <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-3xl mx-auto bg-white p-1 rounded shadow text-center">
-          <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
-        </div>
-        <div className="max-w-3xl mx-auto bg-gray-100 p-2"></div>
-        <div className="max-w-3xl mx-auto bg-white p-6 rounded shadow text-center">
+      <main className="max-w-3xl mx-auto bg-white p-100 rounded shadow py-12 px-4 sm:px-6 lg:px-8">
+      <h1 className="text-5xl font-bold mt-2 mb-5 text-center">Privacy Policy</h1>
+      <p className="text-gray-700">This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our services.</p>
+        <div>
+          <h1 className="text-3xl font-bold mt-10 mb-5">Collection, Use, Disclosure of Information</h1>
           <p className="text-gray-700">
-            This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our services.
+          We collect personal information from you when you provide it to us directly and through your use of the sites. This information may include:
           </p>
+           <li className="text-gray-700 mt-5 ">We may collect personal information that you voluntarily provide to us when you use our services. </li>
+           <li className="text-gray-700">This information may include, but is not limited to, your name, contact information, and payment details. </li>           
+           <li className="text-gray-700">We may use the information collected for various purposes, including but not limited to providing and maintaining our services, improving our services, and contacting you.</li>
+           <li className="text-gray-700">We may disclose your personal information to third parties only in the ways that are described in this Privacy Policy.</li>
+          
         </div>
-
-        <div className="max-w-3xl mx-auto bg-gray-100 p-2"></div>
-
-        <div className="max-w-3xl mx-auto bg-white p-1 rounded shadow text-center">
-          <h1 className="text-3xl font-bold mb-4">Information Collection, Use, Disclosure</h1>
-        </div>
-        <div className="max-w-3xl mx-auto bg-gray-100 p-2"></div>
-        <div className="max-w-3xl mx-auto bg-white p-6 rounded shadow text-center">
-          <p className="text-gray-700">
-           We may collect personal information that you voluntarily provide to us when you use our services. 
-           This information may include, but is not limited to, your name, contact information, and payment details.            
-           We may use the information collected for various purposes, including but not limited to providing and
-           maintaining our services, improving our services, and contacting you. We may disclose your personal information to third parties only in the ways that are described in this
-          Privacy Policy.
-          </p>
-        </div>
-
-        <div className="max-w-3xl mx-auto bg-gray-100 p-2"></div>
-
-        <div className="max-w-3xl mx-auto bg-white p-1 rounded shadow text-center">
-         <h1 className="text-3xl font-bold mb-4">Security</h1>
-        </div>
-        <div className="max-w-3xl mx-auto bg-gray-100 p-2"></div>
-        <div className="max-w-3xl mx-auto bg-white p-6 rounded shadow text-center">
+        <div>
+         <h1 className="text-3xl font-bold mt-10 mb-5">Security</h1>
           <p className="text-gray-700">
             The security of your personal information is important to us, but remember that no method of transmission
             over the internet or electronic storage is 100% secure.
           </p>
         </div>
-
-        <div className="max-w-3xl mx-auto bg-gray-100 p-2"></div>
-
-        <div className="max-w-3xl mx-auto bg-white p-1 rounded shadow text-center">
-          <h1 className="text-3xl font-bold mb-4">Changes to This Privacy Policy</h1>
-        </div>
-        <div className="max-w-3xl mx-auto bg-gray-100 p-2"></div>
-        <div className="max-w-3xl mx-auto bg-white p-6 rounded shadow text-center">
+        <div>
+          <h1 className="text-3xl font-bold mt-10 mb-5">Changes to This Privacy Policy</h1>
           <p className="text-gray-700">
             We may update our Privacy Policy from time to time. You are advised to review this Privacy Policy
             periodically for any changes.
           </p>
         </div>
-
-        <div className="max-w-3xl mx-auto bg-gray-100 p-2"></div>
-
-        <div className="max-w-3xl mx-auto bg-white p-1 rounded shadow text-center">
-         <h1 className="text-3xl font-bold mb-4">Contact Information</h1>
-        </div>
-        <div className="max-w-3xl mx-auto bg-gray-100 p-2"></div>
-        <div className="max-w-3xl mx-auto bg-white p-6 rounded shadow text-center">
+        <div> 
+         <h1 className="text-3xl font-bold mt-10 mb-5">Contact Information</h1>
           <p className="text-gray-700">
             For any questions about this Privacy Policy, please contact us.
           </p>
         </div>
-      </div>
     </main>
   );
 }
--- fe/src/components/Footer/Footer.tsx
+++ fe/src/components/Footer/Footer.tsx
@@ -3,15 +3,13 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="py-6 text-center bg-gray-900 text-white">
-      <div className="flex justify-center">
-        <Link href="/privacypolicy" className="flex items-center">
-            Privacy Policy
-        </Link>
-      </div>
-      <p> </p>
-      <p>&copy; 2023 CarFans</p>
-    </footer>
+    <footer className="py-6 bg-gray-900 text-white flex-col text-center">
+      <Link href="/privacypolicy" className="hover:underline">
+        Privacy Policy
+      </Link>
+    
+    <p>&copy; 2023 CarFans</p>
+  </footer>
   );
 }
 
