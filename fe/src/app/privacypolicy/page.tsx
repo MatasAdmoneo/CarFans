89d36fcb7e33072f526e24a041a9d14e568342fc
AthenaKas <atene.kas@gmail.com>
2023-12-13 20:25:51 +0200

    Privacy Policy try tres

--- fe/src/app/privacypolicy/page.tsx
+++ fe/src/app/privacypolicy/page.tsx
@@ -1,10 +1,10 @@
 function PrivacyPolicy() {
     return (
-      <main className="max-w-3xl mx-auto bg-white p-100 rounded shadow py-12 px-4 sm:px-6 lg:px-8">
+      <main className="mx-auto lg:w-3/4 p-100 rounded py-12 px-4 sm:px-8">
       <h1 className="text-5xl font-bold mt-2 mb-5 text-center">Privacy Policy</h1>
-      <p className="text-gray-700">This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our services.</p>
+      <p className="text-gray-700 text-center">This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our services.</p>
         <div>
-          <h1 className="text-3xl font-bold mt-10 mb-5">Collection, Use, Disclosure of Information</h1>
+          <h2 className="text-3xl font-bold mt-10 mb-5">Collection, Use, Disclosure of Information</h2>
           <p className="text-gray-700">
           We collect personal information from you when you provide it to us directly and through your use of the sites. This information may include:
           </p>
@@ -15,21 +15,21 @@ function PrivacyPolicy() {
           
         </div>
         <div>
-         <h1 className="text-3xl font-bold mt-10 mb-5">Security</h1>
+         <h2 className="text-3xl font-bold mt-10 mb-5">Security</h2>
           <p className="text-gray-700">
             The security of your personal information is important to us, but remember that no method of transmission
             over the internet or electronic storage is 100% secure.
           </p>
         </div>
         <div>
-          <h1 className="text-3xl font-bold mt-10 mb-5">Changes to This Privacy Policy</h1>
+          <h2 className="text-3xl font-bold mt-10 mb-5">Changes to This Privacy Policy</h2>
           <p className="text-gray-700">
             We may update our Privacy Policy from time to time. You are advised to review this Privacy Policy
             periodically for any changes.
           </p>
         </div>
         <div> 
-         <h1 className="text-3xl font-bold mt-10 mb-5">Contact Information</h1>
+         <h2 className="text-3xl font-bold mt-10 mb-5">Contact Information</h2>
           <p className="text-gray-700">
             For any questions about this Privacy Policy, please contact us.
           </p>
