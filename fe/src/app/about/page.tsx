9f7d4d3ef4d5187494c2ddc1f6a6856859e7fdad
Ech0ke <aidas.zygas@gmail.com>
2023-12-19 10:27:04 +0200

    Fix styling on input boxes, add Link instead of a

--- fe/src/app/about/page.tsx
+++ fe/src/app/about/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const AboutUsPage = () => {
   return (
     <div className="bg-gray-800 text-white">
@@ -7,7 +9,12 @@ const AboutUsPage = () => {
             src="https://res.cloudinary.com/value-penguin/image/upload/c_lfill/dpr_1.0/f_auto/h_800/q_auto/w_800/v1/mother-daughter-car_qbsrc9"
             alt="About Us Image"
             className="transition-transform transform hover:scale-105 duration-300"
-            style={{ filter: 'brightness(80%)', objectFit: 'cover', width: '100%', height: '100%' }}
+            style={{
+              filter: "brightness(80%)",
+              objectFit: "cover",
+              width: "100%",
+              height: "100%",
+            }}
           />
           <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
           <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-bold text-white z-10">
@@ -16,20 +23,28 @@ const AboutUsPage = () => {
         </div>
         <div className="max-w-3xl mx-auto text-xl leading-relaxed">
           <p className="mb-6">
-            Currently, in the car repair market, drivers waste time searching for suitable auto repair services, and communication between drivers and service centers can lead to misunderstandings and potential loss of customers.
+            Currently, in the car repair market, drivers waste time searching
+            for suitable auto repair services, and communication between drivers
+            and service centers can lead to misunderstandings and potential loss
+            of customers.
           </p>
           <p className="mb-6">
-            Our system simplifies the car registration process, providing drivers with easily understandable information about repair services. We supply service centers with the necessary information, enabling them to serve customers more quickly and efficiently. Our goal is to optimally align the needs of drivers and service centers, ensuring a smooth and effective collaboration.
+            Our system simplifies the car registration process, providing
+            drivers with easily understandable information about repair
+            services. We supply service centers with the necessary information,
+            enabling them to serve customers more quickly and efficiently. Our
+            goal is to optimally align the needs of drivers and service centers,
+            ensuring a smooth and effective collaboration.
           </p>
         </div>
       </main>
       <section className="py-8 text-center bg-gray-700 text-white">
         <h2 className="text-3xl font-bold">Contact Us</h2>
         <p className="mt-4 text-lg">
-          Have questions or want to get in touch? Visit our{' '}
-          <a href="/contact" className="text-blue-500 hover:underline">
+          Have questions or want to get in touch? Visit our{" "}
+          <Link href="/contact" className="text-blue-500 hover:underline">
             Contact Page
-          </a>
+          </Link>
           .
         </p>
       </section>
--- fe/src/app/contact/page.tsx
+++ fe/src/app/contact/page.tsx
@@ -1,36 +1,38 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 import { Card, Input, Button, Typography } from "@/lib/materialTailwindExports";
 import toast from "react-hot-toast";
+import Link from "next/link";
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
   });
 
-  const handleFormSubmit = async () => {
-    toast.success("Message sent successfully!");
+  const handleFormSubmit = () => {
+    setFormData({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
 
-    setTimeout(() => {
-      setFormData({
-        name: '',
-        email: '',
-        subject: '',
-        message: '',
-      });
-    }, 2000);
+    toast.success("Message sent successfully!");
   };
 
   return (
     <div className="bg-gray-800 text-white flex h-screen p-8 md:p-16">
       <main className="flex-1 py-12 text-center md:text-left">
-        <h1 className="text-5xl md:text-7xl font-bold text-center mb-8 md:mb-12">Contact Us</h1>
+        <h1 className="text-5xl md:text-7xl font-bold text-center mb-8 md:mb-12">
+          Contact Us
+        </h1>
         <div className="max-w-2xl mx-auto text-lg md:text-xl leading-relaxed">
           <p className="mb-8">
-            We{'\''}re here to assist you! Feel free to reach out if you have any questions, suggestions, or just want to say hello.
+            We{"'"}re here to assist you! Feel free to reach out if you have any
+            questions, suggestions, or just want to say hello.
           </p>
           <p className="mb-8">
             Connect with us through the following channels:
@@ -38,11 +40,21 @@ const ContactPage = () => {
           <ul className="list-none mb-8 pl-0">
             <li className="mb-6 flex items-center">
               <strong className="mr-4">Facebook:</strong>
-              <a href="https://facebook.com/CarFans" className="text-blue-500 hover:underline">https://facebook.com/CarFans</a>
+              <Link
+                href="https://facebook.com/CarFans"
+                className="text-blue-500 hover:underline"
+              >
+                https://facebook.com/CarFans
+              </Link>
             </li>
             <li className="mb-6 flex items-center">
               <strong className="mr-4">Twitter:</strong>
-              <a href="https://twitter.com/CarFans" className="text-blue-500 hover:underline">https://twitter.com/CarFans</a>
+              <Link
+                href="https://twitter.com/CarFans"
+                className="text-blue-500 hover:underline"
+              >
+                https://twitter.com/CarFans
+              </Link>
             </li>
             <li className="flex items-center">
               <strong className="mr-4">Phone:</strong>
@@ -50,47 +62,64 @@ const ContactPage = () => {
             </li>
             <li className="flex items-center ">
               <br />
-              <span className="text-gray-500 mb-2">Or send us a message</span>
-              <span className="ml-2 text-2xl block">&#10140;</span>
+              <h2 className="text-2xl md:text-5xl font-bold text-center mt-7">
+                Or send us a message &#10140;
+              </h2>
+              <span className="ml-2 text-2xl block"></span>
             </li>
           </ul>
         </div>
       </main>
 
-      <Card color="white" className="p-8 md:p-16 rounded-lg w-full md:w-1/2 lg:w-1/3 ml-10">
-        <Typography variant="h1" color="blue-gray" className="mb-8">Send us a Message</Typography>
+      <Card
+        color="white"
+        className="p-8 md:p-16 rounded-lg w-full md:w-1/2 lg:w-1/3 ml-10"
+      >
+        <Typography variant="h1" color="blue-gray" className="mb-8">
+          Send us a Message
+        </Typography>
         <form className="grid grid-cols-1 gap-8">
           <Input
             size="lg"
-            placeholder="Your Name"
-            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+            color="blue-gray"
+            label="Your name"
+            name="name"
             crossOrigin=""
             value={formData.name}
             onChange={(e) => setFormData({ ...formData, name: e.target.value })}
           />
           <Input
             size="lg"
-            placeholder="Your Email"
-            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+            color="blue-gray"
+            label="Your email"
+            name="email"
             crossOrigin=""
             value={formData.email}
-            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
           />
           <Input
             size="lg"
-            placeholder="Subject"
-            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+            color="blue-gray"
+            label="Subject"
+            name="subject"
             crossOrigin=""
             value={formData.subject}
-            onChange={(e) => setFormData({ ...formData, subject: e.target.value })}
+            onChange={(e) =>
+              setFormData({ ...formData, subject: e.target.value })
+            }
           />
           <Input
             size="lg"
-            placeholder="Your Message"
-            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+            color="blue-gray"
+            label="Your Message"
+            name="message"
             crossOrigin=""
             value={formData.message}
-            onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+            onChange={(e) =>
+              setFormData({ ...formData, message: e.target.value })
+            }
           />
           <Button
             type="button"
